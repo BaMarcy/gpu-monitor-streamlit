e29b04db3e178c4b36d3addfdf244652b0ae3e71
unknown <balogh.marcell.dk@gmail.com>
2022-07-28 14:54:05 +0200

    merge 4

--- monitor_app.py
+++ monitor_app.py
@@ -6,49 +6,10 @@ Created on Tue Jul 19 10:21:26 2022
 @author: remionette
 """
 
-from flask import Flask, jsonify, request
+
 import sqlite3
-from datetime import datetime
-import json
 import streamlit as st
 import pandas as pd
-
-
-app = Flask(__name__)
-con = sqlite3.connect('data.db', check_same_thread=False)
-cur = con.cursor()
-
-@app.route('/gpu', methods=["POST"])
-def monitor_GPU():
-    data = json.loads(request.data)
-    # str, str, str, str, real, real, real, real
-    l  =[
-        datetime.now(),
-        data["name"],
-        data["driver"], 
-        data["memoryTotal"], 
-        data["memoryFree"], 
-        data["memoryUsed"], 
-        data["temperature"]
-        ]
-    print(data["table"])
-    try:
-        sql = '''INSERT INTO {} (date, name, driver, memoryTotal, memoryFree, memoryUsed, temperature)
-                  VALUES ( ? , ? , ? , ? , ? , ? , ?)'''.format(data["table"])
-        cur.execute(sql, l)
-    except:
-        sql = '''CREATE TABLE IF NOT EXISTS {} (date text, name text, driver text, memoryTotal real, memoryFree real, 
-                    memoryUsed real, temperature real)'''.format(data["table"])
-        cur.execute(sql)
-    con.commit()
-    return jsonify({'result': 'OK'})
-
-
-
-app.run(threaded=True, host='0.0.0.0', port=6666)
-
-
-""" 
 
 con = sqlite3.connect('data.db', check_same_thread=False)
 cur = con.cursor()
@@ -71,4 +32,3 @@ if st.button('Refresh'):
     st.download_button(label="Download data as CSV", data=df.to_csv().encode('utf-8'), file_name='data.csv', mime='text/csv')
     st.area_chart(df['temperature'])
     st.area_chart(df['memoryUsed'])
- """
